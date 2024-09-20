5b5d8a787dac1ef05419772e4f365cf8f34627e1
ChristianMcNachtan <christian.mcnachtan@gmail.com>
2024-09-20 13:40:31 -0400

    Enable creating users

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -7,18 +7,18 @@ import options from "../auth/[...nextauth]/options";
 
 export async function POST(request: NextRequest){
 
-    const session = await getServerSession(options)
+    // const session = await getServerSession(options)
 
-    if (!session) {
-        return NextResponse.json({error: "Unauthorized"}, {status: 401})
-    }
+    // if (!session) {
+    //     return NextResponse.json({error: "Unauthorized"}, {status: 401})
+    // }
 
-    if (session.user.role !== "ADMIN") {
-        return NextResponse.json({error: "Not Admin"}, {status: 401})
-    }
+    // if (session.user.role !== "ADMIN") {
+    //     return NextResponse.json({error: "Not"}, {status: 401})
+    // }
 
 
-console.log(session);
+
 
     const body = await request.json()
     const validation = userSchema.safeParse(body)
